f1e37840cec726d36684423eba95f0c8d0bf1413
Alexander Teusz <teusz.alexander@gmail.com>
2020-03-28 16:41:48 +0100

    refactoring

--- src/categorize_en.py
+++ src/categorize_en.py
@@ -4,32 +4,22 @@ import json
 import os
 
 
-###############################################################################
-# TODO
-# Eventuell Firmennamen etc. abfangen? als Sonstige oder so?
-# Length of sentence
-#
-# Example: 
-# text = "Dude. Amazon, Apple, Microsoft, Germany, Italy, Mafia, Pizza."
-# doc = textacy.Doc(text)
-# result = list(textacy.extract.named_entities(doc, exclude_types='numeric'))
-###############################################################################
-
 def categorizeText(input_text: str):
     """
     :Returns: List = [MainLevel, Difficulty] (some sort of language level)
     """
 
-    #TODO, handle empty texts
     if (not(isinstance(input_text, str)) or (len(input_text) <= 0)):
         dicti = {"unknown": "NOT OKAY!", "A1": "THIS!", "A2" : "IS!", "B1": "NOT!", "B2": "A!", "C1": "TEXT!", "C2": "NO!"}
         return ["NO!", "NO!", dicti]
     
     # store words of text lowercase in list
     words: list = [item.lower() for item in input_text.split()]
+    print("text: ", words)
 
-    # Dict-Counter, der Worte
-    dict_vocab_count = countDicts(words)
+    # count frequency of word in text
+    word_frequency: dict = getWordFrequency(words)
+    print("dict vocab: ", word_frequency)
 
     # Dataframe, set der Worte mit Sprachniveau
     # word, level
@@ -46,10 +36,12 @@ def categorizeText(input_text: str):
     #für jedes Wort aus dem Text, geordnet nach Level, 
     for lvl in ["unknown", "A1", "A2", "B1", "B2", "C1", "C2"]:
         for word in set_word_table.loc[set_word_table['level']== lvl, "word"]:
-            tmp_count += dict_vocab_count[word]
+            tmp_count += word_frequency[word]
         tmp_result = tmp_count/ len(words) * 100
         verteilung[lvl] = round(tmp_result)
         tmp_count = 0
+
+    print("Verteilung: ", verteilung)
     
     #Einstufung anhand des höchsten Levels, das mehr als n verschiedene Wörter enthält
     # sehr unschön bisher!
@@ -84,7 +76,7 @@ def categorizeText(input_text: str):
     return [max_level, difficulty, verteilung]
 
 
-def countDicts(words: list) -> dict:
+def getWordFrequency(words: list) -> dict:
     """
     :Return: dictionary with word and count
     """
@@ -127,7 +119,7 @@ def getWordLevelDataFrameForText(text):
                 else:
                     level = "unknown"
 
-        # add ro: WORD LEVEL
+        # add row WORD LEVEL
         word_level_table = word_level_table.append(
             pd.DataFrame(
                 [
